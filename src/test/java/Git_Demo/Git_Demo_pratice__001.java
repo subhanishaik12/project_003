d2aa8f14ea54b0370eeedf324c33d887048d8dd1
SUBHANI <SUBHANI@subhaniconnects>
2023-10-30 10:16:26 +0530

    commit and push

---
+++ src/test/java/Git_Demo/Git_Demo_pratice__001.java
@@ -0,0 +1,12 @@
+package Git_Demo;
+
+public class Git_Demo_pratice__001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo_Project_");
+
+	}
+
+}
+
